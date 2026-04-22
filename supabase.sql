921f896916175854fa033ac811a5048bbf53badb
B. Peter <bgtptr@protonmail.com>
2026-04-22 20:24:07 +0200

    Add database file.

---
+++ supabase.sql
@@ -0,0 +1,83 @@
+-- WARNING: This schema is for context only and is not meant to be run.
+-- Table order and constraints may not be valid for execution.
+
+CREATE TABLE public.customers (
+  id uuid NOT NULL,
+  stripe_customer_id text,
+  CONSTRAINT customers_pkey PRIMARY KEY (id),
+  CONSTRAINT customers_id_fkey FOREIGN KEY (id) REFERENCES auth.users(id)
+);
+CREATE TABLE public.liked_songs (
+  user_id uuid NOT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  song_id uuid NOT NULL,
+  CONSTRAINT liked_songs_pkey PRIMARY KEY (user_id, song_id),
+  CONSTRAINT liked_songs_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id),
+  CONSTRAINT liked_songs_song_id_fkey FOREIGN KEY (song_id) REFERENCES public.songs(id)
+);
+CREATE TABLE public.prices (
+  id text NOT NULL,
+  product_id text,
+  active boolean,
+  description text,
+  unit_amount bigint,
+  currency text CHECK (char_length(currency) = 3),
+  type USER-DEFINED,
+  interval USER-DEFINED,
+  interval_count integer,
+  trial_period_days integer,
+  metadata jsonb,
+  CONSTRAINT prices_pkey PRIMARY KEY (id),
+  CONSTRAINT prices_product_id_fkey FOREIGN KEY (product_id) REFERENCES public.products(id)
+);
+CREATE TABLE public.products (
+  id text NOT NULL,
+  active boolean,
+  name text,
+  description text,
+  image text,
+  metadata jsonb,
+  CONSTRAINT products_pkey PRIMARY KEY (id)
+);
+CREATE TABLE public.songs (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  title text,
+  song_path text,
+  image_path text,
+  author text,
+  user_id uuid NOT NULL,
+  bpm integer,
+  duration integer,
+  CONSTRAINT songs_pkey PRIMARY KEY (id),
+  CONSTRAINT songs_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id)
+);
+CREATE TABLE public.subscriptions (
+  id text NOT NULL,
+  user_id uuid NOT NULL,
+  status USER-DEFINED,
+  metadata jsonb,
+  price_id text,
+  quantity integer,
+  cancel_at_period_end boolean,
+  created timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
+  current_period_start timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
+  current_period_end timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
+  ended_at timestamp with time zone DEFAULT timezone('utc'::text, now()),
+  cancel_at timestamp with time zone DEFAULT timezone('utc'::text, now()),
+  canceled_at timestamp with time zone DEFAULT timezone('utc'::text, now()),
+  trial_start timestamp with time zone DEFAULT timezone('utc'::text, now()),
+  trial_end timestamp with time zone DEFAULT timezone('utc'::text, now()),
+  CONSTRAINT subscriptions_pkey PRIMARY KEY (id),
+  CONSTRAINT subscriptions_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id),
+  CONSTRAINT subscriptions_price_id_fkey FOREIGN KEY (price_id) REFERENCES public.prices(id)
+);
+CREATE TABLE public.users (
+  id uuid NOT NULL,
+  full_name text,
+  avatar_url text,
+  billing_address jsonb,
+  payment_method jsonb,
+  CONSTRAINT users_pkey PRIMARY KEY (id),
+  CONSTRAINT users_id_fkey FOREIGN KEY (id) REFERENCES auth.users(id)
+);
